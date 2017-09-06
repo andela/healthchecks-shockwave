bf3f3a778cc44da75ceff4488281aad10ad43344
Ezekiel Mugaya <emugaya@gmail.com>
2017-09-06 22:32:28 +0300

    feat(pause-test): Pause tests

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -11,8 +11,8 @@ class PauseTestCase(BaseTestCase):
         url = "/api/v1/checks/%s/pause" % check.code
         r = self.client.post(url, "", content_type="application/json",
                              HTTP_X_API_KEY="abc")
-
         ### Assert the expected status code and check's status
+        assert r.status_code == 200
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -25,3 +25,12 @@ class PauseTestCase(BaseTestCase):
         self.assertEqual(r.status_code, 400)
 
         ### Test that it only allows post requests
+    def test_that_it_only_allows_post_requests(self):
+        check = Check(user=self.alice, status="up")
+        check.save()
+
+        url = "/api/v1/checks/%s/pause" % check.code
+        resp = self.client.get(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+
+        assert resp.status_code == 405
