308ee2307e79f9c9f5f2ae2e0815b2c42793a9a3
WinstonKamau <crispus.kamau@andela.com>
2017-09-07 12:12:18 +0300

    feat(checks): Test team access only allowed to members
    Ensure that team access on checks added is only allowed for team members, who can view checks added by members.
    
    Team acces to non-team members is not allowed, and non-team members won't view it.
    
    [Fixes #150627360]

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -12,3 +12,25 @@ class AddCheckTestCase(BaseTestCase):
         assert Check.objects.count() == 1
 
     ### Test that team access works
+    def test_team_access(self):
+        """Test that team members can view all added checks, added by any
+        member in the team while non-members cannot.
+        Alice the team lead adds a check. Bob a team member of Alice's team
+        opens his page and views a check Alice added on his page. Charlie a
+        non-team member opens his page and does not have a view of a check.
+        """
+        url1 = "/checks/add/"
+        url2 = "/checks/"
+        self.client.login(username="alice@example.org", password="password")
+        post_check_alice = self.client.post(url1)
+        self.assertRedirects(post_check_alice, "/checks/")
+        self.client.logout()
+        value = Check.objects.all()
+        alices_check = list(value)[0]
+        self.client.login(username="bob@example.org", password="password")
+        bobs_checks = self.client.get(url2)
+        self.assertContains(bobs_checks, alices_check.code)
+        self.client.logout()
+        self.client.login(username="charlie@example.org", password="password")
+        charlies_checks = self.client.get(url2)
+        self.assertNotContains(charlies_checks, alices_check.code)
