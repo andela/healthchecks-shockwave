edecb1aa8bc7fd7443c355ab213145b965c1994a
Male Michael <michael.male@andela.com>
2017-09-11 12:55:27 +0300

    Merge branch 'development' of https://github.com/andela/healthchecks-shockwave into development

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -36,42 +36,4 @@ class CheckModelTestCase(TestCase):
         check.status = "new"
         self.assertFalse(check.in_grace_period())
 
-    def test_update_status_often_to_up(self):
-        """
-        Test that get_status updates status often to up when duration since the last
-        ping exceeds the time set at reverse grace period
-        """
-        check = Check()
-        check.status = "often"
-        check.last_ping = timezone.now() - timedelta(hours=23, minutes=1)
-        self.assertEqual(check.get_status(), "up")
-
-    def test_ping_often_returns_status_often(self):
-        """
-        Test to check that if a ping is sent with in before the reverse grace period
-        the  method ping_often returns status often and sends an alert
-        """
-        check = Check()
-        check.status = "up"
-        check.last_ping = timezone.now() - timedelta(hours=22, minutes=30)
-        check.save()
-        self.assertEqual(check.ping_often(), "often")
-
-    def test_ping_often_returns_status_up(self):
-        """
-        Test that a ping sent with in and after reverse grace period would return
-        up
-        """
-        check = Check()
-        check.status = "often"
-        check.last_ping = timezone.now() - timedelta(hours=23, minutes=30)
-        self.assertEqual(check.ping_often(), "up")
-
-    def test_ping_often_returns_original_status(self):
-        """
-        Test to check that if a ping is sent with status new or paused then that same status
-        is returned
-        """
-        check = Check()
-        check.status = "new"
-        self.assertEqual(check.ping_often(), "new")
+    ### Test that when a new check is created, it is not in the grace period
