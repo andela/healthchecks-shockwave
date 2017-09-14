7c53cd17f000aa14b9aa433819ce5af31d2f5fda
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2017-09-14 10:22:57 +0300

    [Feature #150627362] Write test for nagging Test that next_nag is updated Change nag icon colors

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -28,7 +28,7 @@ class SendAlertsTestCase(BaseTestCase):
             handled_names.append(args[0].name)
 
         assert set(names) == set(handled_names)
-        ### The above assert fails. Make it pass
+        ## The above assert fails. Make it pass
 
     def test_it_handles_grace_period(self):
         check = Check(user=self.alice, status="up")
@@ -37,6 +37,21 @@ class SendAlertsTestCase(BaseTestCase):
         check.save()
 
         # Expect no exceptions--
-        Command().handle_one(check)
+        Command().handle_many()
+
+    @patch("hc.api.management.commands.sendalerts.Command.handle_one")
+    def test_it_changes_next_nag_after_notification(self, mock):
+        check = Check(user=self.alice, status="down")
+        check.nag_time = timedelta(minutes=3)
+        check.grace = timedelta(minutes=1)
+        check.timeout = timedelta(minutes=1)
+        now = timezone.now()
+        check.next_nag = now
+        check.last_ping = now - timedelta(minutes=10)
+        check.save()
+        Command().handle_many()
+        checks = Check.objects.filter(status="down", nag_mode=True).first()
+        next_time = now + timedelta(minutes=3)
+        assert checks.next_nag >= next_time
 
     ### Assert when Command's handle many that when handle_many should return True
