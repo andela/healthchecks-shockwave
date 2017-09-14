50a3f213e4e3441b08f9117f7b17691122502d6c
Ezekiel Mugaya <emugaya@gmail.com>
2017-09-14 17:17:19 +0300

    Feature(150627370)Help Center. Added Faq, and Video models

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from embed_video.fields import EmbedVideoField
 
 # Create your models here.
+class Faq(models.Model):
+	question = models.CharField(max_length=200)
+	answer = models.TextField()
+	created_date = models.DateTimeField(default = timezone.now)
+
+	def __str__(self):
+		return self.question
+
+class Video(models.Model):
+	title = models.CharField(max_length=200)
+	video_url = EmbedVideoField()
+	created_date = models.DateTimeField(default = timezone.now)
+
+	def __str__(self):
+		return self.title
