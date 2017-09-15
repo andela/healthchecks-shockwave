25228a1a7ea4d87426a9c4bba42ee0ea13b7645f
Ezekiel Mugaya <emugaya@gmail.com>
2017-09-15 09:52:27 +0300

    Changed model name from Post to BlogPost

--- hc/blogs/tests/test_blog_post.py
+++ hc/blogs/tests/test_blog_post.py
@@ -2,17 +2,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from hc.test import BaseTestCase
 from django.views.generic import ListView
-from hc.blogs.models import Post
-from hc.blogs.forms import PostForm
-# from hc.blogs.views import post
+from hc.blogs.models import BlogPost
+from hc.blogs.forms import BlogPostForm
 
 class BlogTestCase(BaseTestCase):
 	URL = "/blog/"
 	def setUp(self):
 		super(BlogTestCase, self).setUp()
-		Post.objects.create(title="Trravelling",text="Nairobi", \
+		BlogPost.objects.create(title="Trravelling",text="Nairobi", \
 				tags="Cities", author=self.alice)
-		Post.objects.create(title="Teams",text="IT Teams", \
+		BlogPost.objects.create(title="Teams",text="IT Teams", \
 				tags="Tech", author=self.alice)
 		self.URL = "/blog/"
 
@@ -21,32 +20,32 @@ class BlogTestCase(BaseTestCase):
 		self.assertEquals(response.status_code, 200)
 
 	def test_create_blog_post(self):
-		Post.objects.create(title="Trravelling",text="Nairobi", \
+		BlogPost.objects.create(title="Trravelling",text="Nairobi", \
 				tags="Cities", author=self.alice)
 		response = self.client.get(self.URL)
 		self.assertEquals(response.status_code, 200)
-		post_id = str(Post.objects.last().pk)
-		post = Post.objects.all()
+		post_id = str(BlogPost.objects.last().pk)
+		post = BlogPost.objects.all()
 		self.assertEquals(len(post), 3)
 		response = self.client.get(self.URL+'post/' + post_id +'/')
 		self. assertEquals(response.status_code, 200)
 
 	def test_update_blog_post(self):
-		post = Post.objects.first()
+		post = BlogPost.objects.first()
 		post_id = post.id
 		post.title = "Travelling to Kenya"
 		post.text = "Visit Nairobi"
 		post.save()
-		post = Post.objects.get(id=post_id)
+		post = BlogPost.objects.get(id=post_id)
 		self.assertEquals(post.title, "Travelling to Kenya")
 		self.assertEquals(post.text, "Visit Nairobi")
 
 	def test_delete_blog_post(self):
-		post =Post.objects.all()
+		post = BlogPost.objects.all()
 		num_posts_before_print = len(post)
-		post = Post.objects.first()
+		post = BlogPost.objects.first()
 		post.delete()
-		self.assertEquals(len(Post.objects.all()), 1)
+		self.assertEquals(len(BlogPost.objects.all()), 1)
 		
 
 
