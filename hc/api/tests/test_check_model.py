cdbbd3a6388d996db96cf5e498716ff819e67b74
AnthonyGW <anthony.waithaka@andela.com>
2017-09-15 21:07:53 +0300

    fix(api_tests): fix sources of merge conflicts
    - replace comments with code from development branch
    
    [Fixes #150627367]

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -13,6 +13,8 @@ class CheckModelTestCase(TestCase):
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
         ### Repeat above test for when check is an empty string
+        check.tags = "  "
+        self.assertEqual(check.tags_list(), [])
 
     def test_status_works_with_grace_period(self):
         check = Check()
@@ -36,3 +38,47 @@ class CheckModelTestCase(TestCase):
         self.assertFalse(check.in_grace_period())
 
     ### Test that when a new check is created, it is not in the grace period
+    def test_new_check_is_not_in_grace_period(self):
+        check = Check()
+        check.status = "new"
+        self.assertFalse(check.in_grace_period())
+
+    def test_update_status_often_to_up(self):
+        """
+        Test that get_status updates status often to up when duration since the last
+        ping exceeds the time set at reverse grace period
+        """
+        check = Check()
+        check.status = "often"
+        check.last_ping = timezone.now() - timedelta(hours=23, minutes=1)
+        self.assertEqual(check.get_status(), "up")
+
+    def test_ping_often_returns_status_often(self):
+        """
+        Test to check that if a ping is sent with in before the reverse grace period
+        the  method ping_often returns status often and sends an alert
+        """
+        check = Check()
+        check.status = "up"
+        check.last_ping = timezone.now() - timedelta(hours=22, minutes=30)
+        check.save()
+        self.assertEqual(check.ping_often(), "often")
+
+    def test_ping_often_returns_status_up(self):
+        """
+        Test that a ping sent with in and after reverse grace period would return
+        up
+        """
+        check = Check()
+        check.status = "often"
+        check.last_ping = timezone.now() - timedelta(hours=23, minutes=30)
+        self.assertEqual(check.ping_often(), "up")
+
+    def test_ping_often_returns_original_status(self):
+        """
+        Test to check that if a ping is sent with status new or paused then that same status
+        is returned
+        """
+        check = Check()
+        check.status = "new"
+        self.assertEqual(check.ping_often(), "new")
