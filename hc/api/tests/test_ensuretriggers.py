f96058625be8494d491a809d4005f18964eff89d
AnthonyGW <anthony.waithaka@andela.com>
2017-09-15 21:07:53 +0300

    fix(api_tests): fix sources of merge conflicts
    - replace comments with code from development branch
    
    [Fixes #150627367]

--- hc/api/tests/test_ensuretriggers.py
+++ hc/api/tests/test_ensuretriggers.py
@@ -27,3 +27,40 @@ class EnsureTriggersTestCase(TestCase):
         check.save()
         check.refresh_from_db()
         ### Assert that alert_after is lesser than the check's alert_after 
+        self.assertLess(alert_after, check.alert_after)
+
+    def test_ensure_triggers_alert_before(self):
+        """
+        Tests that alert before is always reset after every ping
+        """
+        Command().handle()
+
+        check = Check.objects.create()
+        assert check.alert_before is None
+
+        check.last_ping = timezone.now()
+        check.save()
+        check.refresh_from_db()
+        assert check.alert_before is not None
+
+        alert_before = check.alert_before
+
+        check.last_ping += timedelta(hours=22)
+        check.save()
+        check.refresh_from_db()
+        self.assertGreater(check.alert_before, alert_before)
+
+    def test_alert_before_set_to_expected_time(self):
+        """
+        Tests that alert before is set to the expected time which would be an addition
+        of the time of last_ping and timeout and the total subtracted by the grace period
+        """
+        Command().handle()
+
+        check = Check.objects.create()
+        assert check.alert_before is None
+
+        check.last_ping = timezone.now()
+        check.save()
+        check.refresh_from_db()
+        self.assertEqual(check.alert_before, (check.last_ping+check.timeout-check.grace))
